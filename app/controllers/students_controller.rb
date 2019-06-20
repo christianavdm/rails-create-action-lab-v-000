b0578511d1b2f5ba578b858d02b5bac78d2b0f5d
Christiana Vandermale <christianavdm@gmail.com>
2019-06-20 21:40:51 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,7 +15,7 @@ class StudentsController < ApplicationController
     @student = Student.new
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
-    @student.save 
+    @student.save
     redirect_to_student_path(@student)
   end
 
